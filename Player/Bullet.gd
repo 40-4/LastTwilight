0aff93c13b4ac4a51fe0aefbfac9f07653c9f69d
BhanosTD <93290880+BhanosTD@users.noreply.github.com>
2023-12-02 13:24:18 +0100

    Revert "Merge remote-tracking branch 'origin/gabriel-test' into ksawery/sprity"
    This reverts commit dc3e1ad7dd08724f46face92221e1b7631328cf5, reversing
    changes made to 3a39823c44c3474d79dc1042e021f180c98e232f.

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
 var speed : float = 250.0
-var damage = 5
+
 var direction : Vector2 = Vector2.ZERO
-var boom = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,10 +18,7 @@ func _physics_process(delta):
 		for ob in get_overlapping_bodies():
 			if ob.has_meta("type"):
 				if ob.get_meta("type") == "enemy":
-					for i in get_tree().get_nodes_in_group("enemies"):
-						if global_position.distance_to(i.global_position) <= 50 and boom == true:
-							i.damage(damage)
-					ob.damage(damage)
+					ob.damage(5)
 				if ob.get_meta("type") != "player":
 					queue_free()
 			else:
--- singletones/global_variables.gd
+++ singletones/global_variables.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var totems = {1:"res://Totems/totem_of_light.tscn", 2:"res://totemy/totem2.tscn", 3:"res://Totems/totem_of_power.tscn", 5:"res://Totems/totem_of_destruction.tscn" }
+var totems = {1:"res://totemy/totem1.tscn", 2:"res://totemy/totem2.tscn", 3:"res://totemy/totem3.tscn" }
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
